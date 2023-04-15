d0443df601ef81852bdb001b7a219279ad5a1278
krenol <lauritzgroeger@gmail.com>
2023-04-15 18:58:45 +0200

    log cleanup

--- backend/src/middleware/authenticated.ts
+++ backend/src/middleware/authenticated.ts
@@ -4,13 +4,12 @@ import { LOGGER } from "services/loggers";
 
 export const checkAuthenticated = async (req: Request, res: Response, next: NextFunction) => {
     LOGGER.info("Check if request is authenticated");
-    LOGGER.warn(JSON.stringify(req.session.cookie));
+    LOGGER.debug(JSON.stringify(req.session.cookie));
     if (req.isAuthenticated()) {
         LOGGER.info("Authenticated request - continuing");
         req.session.touch();
         return next();
     }
-    LOGGER.warn(JSON.stringify(req.user));
     LOGGER.error("Unauthenticated request - throwing error");
     return next(new ApiError("User not authenticated", new Error("User not authenticated"), 401));
 }

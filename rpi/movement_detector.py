b6762964ff624e94f661ff3eeb0499f0b49af36c
krenol <lauritzgroeger@gmail.com>
2023-04-15 17:24:11 +0200

    use classes in py

--- rpi/movement_detector.py
+++ rpi/movement_detector.py
@@ -1,46 +1,52 @@
 import subprocess
 import RPi.GPIO as GPIO
 import time
+from enum import Enum
 
 sensorPin = 11  # define the sensorPin
-currentScreenState = "off"
 
 
-def setup():
-    print("Starting")
-    GPIO.setmode(GPIO.BOARD)  # Numbers GPIOs by physical location
-    GPIO.setup(sensorPin, GPIO.IN)  # Set sensorPin's mode is input
+class DisplayState(str, Enum):
+    on = 'on'
+    off = 'off'
+
+
+class ScreenToggle:
+    def __init__(self):
+        self._currentScreenState = "off"
+
+    def setScreenState(self, state: DisplayState) -> None:
+        if self._currentScreenState != state:
+            subprocess.run(
+                ["xset", "-display", ":0.0", "dpms", "force", state])
+            self._currentScreenState = state
+
+
+class MotionSensor:
+    def __init__(self, pin: int):
+        self._pin = pin
+        GPIO.setup(self._pin, GPIO.IN)
+
+    def motionIsDetected(self) -> bool:
+        return GPIO.input(self._pin) == GPIO.HIGH
 
 
 def loop():
     print("Start loop!")
+    motionSensor = MotionSensor(sensorPin)
+    screenToggle = ScreenToggle()
     while True:
-        if GPIO.input(sensorPin) == GPIO.HIGH:
-            print("Motion detected")
+        if motionSensor.motionIsDetected():
+            screenToggle.toggleScreen("on")
             time.sleep(1)
-            toggleScreen(True)
         else:
-            toggleScreen(False)
-
-
-def toggleScreen(turnOn: bool):
-    stateStr = "off"
-    if turnOn:
-        stateStr = "on"
-    if currentScreenState != stateStr:
-        subprocess.run(
-            ["xset", "-display", ":0.0", "dpms", "force", stateStr])
-        currentScreenState = stateStr
-
-
-def destroy():
-    GPIO.cleanup()  # Release resource
+            screenToggle.toggleScreen("off")
 
 
 if __name__ == '__main__':  # Program start from here
-    setup()
+    GPIO.setmode(GPIO.BOARD)
     try:
         loop()
     # When 'Ctrl+C' is pressed, the child program destroy() will be executed.
     except KeyboardInterrupt:
-        destroy()
+        GPIO.cleanup()

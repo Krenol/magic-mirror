a0e403df06930b6714193c3ad202fa88b80a7b06
krenol <lauritzgroeger@gmail.com>
2023-05-13 16:11:17 +0200

    upgraded packages

--- frontend/src/assets/styles/theme.ts
+++ frontend/src/assets/styles/theme.ts
@@ -1,10 +1,11 @@
-import { createTheme } from "@mui/material/styles";
+import { SxProps, createTheme } from "@mui/material/styles";
+import { Theme } from "@emotion/react"
 export const PADDING = 2
 export const SPACING = 4;
 
-export const smallFontSize = { fontSize: 12.5 }
-export const xSmallFontSize = { fontSize: 10.5 }
-export const boldText = { fontWeight: 600 }
+export const smallFontSize: SxProps<Theme> = { fontSize: 12.5 }
+export const xSmallFontSize: SxProps<Theme> = { fontSize: 10.5 }
+export const boldText: SxProps<Theme> = { fontWeight: 600 }
 
 export const theme = createTheme({
     palette: {

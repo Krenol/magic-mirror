b48de284acb407c35d6b3a0b4e7c428bec71e86c
krenol <lauritzgroeger@gmail.com>
2023-06-26 22:09:59 +0200

    small cleanup

--- backend/src/routes/auth/api/index.ts
+++ backend/src/routes/auth/api/index.ts
@@ -5,15 +5,15 @@ import { getUserInfo } from "routes/auth/services";
 import { IDtoUser } from "models/mongo/users";
 
 export const logout = async (req: Request, res: Response) => {
-    LOGGER.info(`User ${(JSON.stringify(req.user as IDtoUser)) || 'unknown'} signed out`);
+    LOGGER.info(`User ${(req.user as IDtoUser).sub || 'unknown'} signed out`);
     return _logout(req, res)
         .catch(err => LOGGER.error(err.message))
 }
 
 const _logout = async (req: Request, res: Response) => {
-    LOGGER.info(`User ${(JSON.stringify(req.user as IDtoUser)) || 'unknown'} signed out`);
+    LOGGER.info(`User ${(req.user as IDtoUser).sub || 'unknown'} signed out`);
     return req.session.destroy(() => req.logout(() => {
-        LOGGER.info(`User ${(req.user as IDtoUser) || 'unknown'} signed out`);
+        LOGGER.info(`User ${(req.user as IDtoUser).sub || 'unknown'} signed out`);
         return res.status(200).json({ success: true });
     }));
 }
--- backend/src/routes/auth/services/index.ts
+++ backend/src/routes/auth/services/index.ts
@@ -1,31 +1,16 @@
-import { GOOGLE_TOKENINFO_ENDPOINT, GOOGLE_USER_INFO_ENDPOINT } from "config";
+import { GOOGLE_USER_INFO_ENDPOINT } from "config";
 import { ApiError } from "models/api/api_error";
-import { AccessTokenInfo } from "models/api/tokeninfo";
-import { getAccessToken, getAuthenticationHeader } from "services/identity/user";
 import { fetchJsonGoogleAuthRefresh } from "services/fetch";
 import { IDtoUser } from "models/mongo/users";
 import { TResponse } from "models/api/fetch";
 
-export const postTokenInfo = async (user: IDtoUser): Promise<boolean> => {
-    const access_token = await getAccessToken(user);
-    return fetchJsonGoogleAuthRefresh(`${GOOGLE_TOKENINFO_ENDPOINT}?access_token=${access_token}`, user, { method: 'POST' }, GOOGLE_TOKENINFO_ENDPOINT)
-        .then(response => checkTokenInfoResponse(response.body as AccessTokenInfo))
-        .catch(err => { throw err })
-}
-
-const checkTokenInfoResponse = async (tokenInfo: AccessTokenInfo): Promise<boolean> => {
-    if (tokenInfo.expires_in > 0) return true;
-    throw new ApiError("Unauthenticated request", new Error(), 401);
-}
-
 export const getUserInfo = async (user: IDtoUser): Promise<boolean> => {
-    const authHeader = await getAuthenticationHeader(user);
-    return fetchJsonGoogleAuthRefresh(GOOGLE_USER_INFO_ENDPOINT, user, authHeader)
+    return fetchJsonGoogleAuthRefresh(GOOGLE_USER_INFO_ENDPOINT, user)
         .then(response => checkUserInfoResponse(response))
         .catch(err => { throw err })
 }
 
-const checkUserInfoResponse = async (response: TResponse): Promise<boolean> => {
-    if (response.status === 200) return true;
+const checkUserInfoResponse = async (response?: TResponse): Promise<boolean> => {
+    if (response?.status === 200) return true;
     throw new ApiError("Unauthenticated request", new Error(), 401);
 }

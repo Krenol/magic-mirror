91170b6b608a6e5ce939dfa44acd103ffe9ac33b
krenol <lauritzgroeger@gmail.com>
2023-07-12 19:11:29 +0200

    only refetch on dashboard page

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -9,11 +9,12 @@ import Birthdays from '../features/birthdays/Birthdays';
 import HourlyWeather from '../features/hourly_forecast/HourlyForecast';
 import UpcomingEvents from '../features/upcoming_events/UpcomingEvents';
 import { logout } from "../apis/logout"
+import { REFETCH_INTERVAL } from '../constants/api';
 
 export const Dashboard = () => {
   return (
     <React.Fragment>
-      <SessionCheck onUnauthenticated={logout} />
+      <SessionCheck onUnauthenticated={logout} refetchInterval={REFETCH_INTERVAL} />
       <Box sx={dashboardStyle}>
         <Time />
         <Birthdays />

cff97f4821e550a500101b4b839dc96e4c18a930
krenol <lauritzgroeger@gmail.com>
2023-07-23 12:48:42 +0200

    updated error handling

--- backend/src/routes/auth/api/index.ts
+++ backend/src/routes/auth/api/index.ts
@@ -4,17 +4,17 @@ import { LOGGER } from "services/loggers";
 import { getUserInfo } from "routes/auth/services";
 import { IDtoUser } from "models/mongo/users";
 
-export const logout = async (req: Request, res: Response) => {
+export const logout = async (req: Request, res: Response, next: NextFunction) => {
     LOGGER.info(`User ${(req.user as IDtoUser).sub || 'unknown'} is signing out`);
-    return _logout(req, res)
-        .catch(err => LOGGER.error(err.message))
+    return _logout(req, res, next)
+        .catch(err => new ApiError('Error while signing out', err, 500));
 }
 
-const _logout = async (req: Request, res: Response) => {
+const _logout = async (req: Request, res: Response, next: NextFunction) => {
     return req.session.destroy((err: any) => {
         if (err) {
             LOGGER.warn(`Error while signing out user ${(req.user as IDtoUser).sub || 'unknown'}: <${err}>`);
-            return res.status(500).json({ success: false });
+            return next(new ApiError('Error while signing out', err, 500));
         }
         LOGGER.info(`User ${(req.user as IDtoUser).sub || 'unknown'} signed out`);
         return res.status(200).json({ success: true });

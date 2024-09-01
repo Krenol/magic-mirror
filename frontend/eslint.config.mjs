42b5a07c756c8adf30951b9ed063954db3fc8f3b
krenol <lauritzgroeger@gmail.com>
2024-09-01 16:42:21 +0200

    Fixed some missing deps

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,42 +1,58 @@
-import { fixupConfigRules } from "@eslint/compat";
-import reactRefresh from "eslint-plugin-react-refresh";
-import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import { fixupConfigRules } from '@eslint/compat'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
+import tsParser from '@typescript-eslint/parser'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
     baseDirectory: __dirname,
     recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
+    allConfig: js.configs.all,
+})
 
-export default [{
-    ignores: ["**/dist", "**/.eslintrc.cjs"],
-}, ...fixupConfigRules(compat.extends(
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-)), {
-    plugins: {
-        "react-refresh": reactRefresh,
+export default [
+    {
+        ignores: [
+            '**/dist',
+            '**/.eslintrc.cjs',
+            '**/node_modules',
+            '**/nginx',
+            '**/docker',
+            '**/public',
+        ],
     },
-
-    languageOptions: {
-        globals: {
-            ...globals.browser,
+    ...fixupConfigRules(
+        compat.extends(
+            'eslint:recommended',
+            'plugin:@typescript-eslint/recommended',
+            'plugin:react-hooks/recommended'
+        )
+    ),
+    {
+        plugins: {
+            'react-refresh': reactRefresh,
         },
 
-        parser: tsParser,
-    },
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+            },
 
-    rules: {
-        "react-refresh/only-export-components": ["warn", {
-            allowConstantExport: true,
-        }],
+            parser: tsParser,
+        },
+
+        rules: {
+            'react-refresh/only-export-components': [
+                'warn',
+                {
+                    allowConstantExport: true,
+                },
+            ],
+        },
     },
-}];
+]
